5a1cfd3b80d4bc207a710a375fd65ef7684fc136
Edwin Martinez <edwin.martinez11@upr.edu>
2023-05-15 20:40:42 -0400

    Fail Expected to verify email

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 import calculator
 
 def test_add():
-    assert calculator.add(2, 3) == 5
+    assert calculator.add(2, 3) == 6
     assert calculator.add(-1, 1) == 0
     assert calculator.add(0, 0) == 0
     assert calculator.add(2.5, 1.5) == 4.0
